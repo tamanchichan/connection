553903dd51e956149209adcfc42bcd1819d661f6
tamanchichan <tamanchichan@gmail.com>
2023-04-13 12:26:10 -0500

    column-1 created

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,1 +1,13 @@
 'use strict';
+
+localStorage.setItem('name', 'Taman Chi Chan');
+localStorage.setItem('profession', 'Software Developer');
+localStorage.setItem('city', 'Winnipeg');
+
+const username = document.querySelector('.username');
+const profession = document.querySelector('.profession');
+const city = document.querySelector('.city');
+
+username.innerText = localStorage.getItem('name');
+profession.innerText = localStorage.getItem('profession');
+city.innerText = localStorage.getItem('city');
